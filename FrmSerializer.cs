1573bb5b429815526977f63482720ecd54c2baee
Joseph Fischetti <fischettijw@gmail.com>
2020-03-07 19:14:50 -0500

    Add project files.

---
+++ FrmSerializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace cSharpSerializer
+{
+    public partial class FrmSerializer : Form
+    {
+        static List<Sport> sportList = new List<Sport>();
+
+        public FrmSerializer()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSerializer_Load(object sender, EventArgs e)
+        {
+
+            if (!Sport.Contains("Football")) { sportList.Add(new Sport("Football", true, 'M')); }
+            if (!Sport.Contains("Soccer")) { sportList.Add(new Sport("Soccer", true, 'B')); }
+            if (!Sport.Contains("Tennis")) { sportList.Add(new Sport("Tennis", true, 'B')); }
+            if (!Sport.Contains("Baseball")) { sportList.Add(new Sport("Baseball", true, 'M')); }
+            if (!Sport.Contains("Swimming")) { sportList.Add(new Sport("Swimming", false, 'B')); }
+            if (!Sport.Contains("Softball")) { sportList.Add(new Sport("Softball", true, 'F')); }
+            if (!Sport.Contains("Bowling")) { sportList.Add(new Sport("Bowling", false, 'B')); }
+
+
+   
+
+        }
+    }
+}
---
+++ Sport.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace cSharpSerializer
+{
+    public class Sport
+    {
+        public static int numOfSports = 0;
+        public static List<string> sportsList = new List<string>();
+        public string Name { get; set; }
+        public bool Outdoor { get; set; }
+        public char Sex { get; set; }
+
+        public Sport(string name, bool outdoor, char sex)
+        {
+            if (Contains(name)) { return; };
+            numOfSports += 1;
+            Name = name;
+            sportsList.Add(name);
+            Outdoor = outdoor;
+            switch (sex)
+            {
+                case 'M':
+                case 'm':
+                    Sex = 'M';
+                    break;
+                case 'F':
+                case 'f':
+                    Sex = 'F';
+                    break;
+                default:
+                    Sex = 'B';
+                    break;
+            }
+        }
+        public static bool Contains(string name)
+        {
+            if (sportsList.Contains(name))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
+
